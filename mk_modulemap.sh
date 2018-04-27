aeb877df52cdfb6fa3f9feca4bddadf2ac2426c5
继刚 <keith_127@126.com>
2018-04-27 14:08:29 +0800

    Update mk_modulemap.sh

--- mk_modulemap.sh
+++ mk_modulemap.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
+# 脚本出处：https://github.com/madordie/Demo-PodspecUseBMK
 # 自动生成modulemap脚本
 #    接受一个路径list
 #    并遍历list所有的framework,然后补全缺失的module.modulemap
-
 
 # 创建modulemap文件，接收一个后缀为.framework的路经
 function mk_map(){
@@ -12,7 +11,6 @@ function mk_map(){
 
     # 确保该路径为framework
     if [[ $framework =~ ".framework\$" ]]; then
-        echo "isnot framework skip $1"
         return
     fi
 
@@ -22,7 +20,6 @@ function mk_map(){
 
     # 确保没有已存在的module.modulemap
     if [ -f $framework/Modules/module.modulemap ]; then
-        echo "map is installed skip $1"
         return
     fi
     echo "framework module $framework_name {" >> $framework/Modules/module.modulemap
@@ -32,9 +29,7 @@ function mk_map(){
     done
     echo "    export *" >> $framework/Modules/module.modulemap
     echo "}" >> $framework/Modules/module.modulemap
-    echo "$1 done."
 }
-
 
 for path in $* ; do
     for framework in `find $path -name "*.framework"`; do
